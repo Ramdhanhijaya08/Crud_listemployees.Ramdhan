8986bbc068e2998e930a967e30cd455acad55613
Dwi Ramdhan Hijaya <109592397+Ramdhanhijaya08@users.noreply.github.com>
2022-09-12 01:22:07 +0700

    Add files via upload

---
+++ src/components/Employees.js
@@ -0,0 +1,26 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Rehan ardiansyah",
+        Age:"30"
+    },{
+        id:"2",
+        Name:"Ayu cantika",
+        Age:"20"
+    },{
+        id:"3",
+        Name:"Dwi Ramdhan Hijaya",
+        Age:"18"
+    },
+    {
+        id:"4",
+        Name:"Jamilah",
+        Age:"20"
+    },{
+        id:"5",
+        Name:"Kiki Saputri",
+        Age:"25"
+    }
+]
+
+export default Employees;
